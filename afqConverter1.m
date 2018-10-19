93dd9db95ca18004edb9f769889bb996fdfbd1aa
Giulia Berto <gberto@hm012.karst.uits.iu.edu>
2018-10-19 09:33:19 -0400

    update with correct fuction for conversion

--- afqConverter1.m
+++ afqConverter1.m
@@ -1,9 +1,10 @@
 function out = afqConverter1()
 
-addpath(genpath('/N/u/hayashis/BigRed2/git/vistasoft'));
-addpath(genpath('/N/u/brlife/git/jsonlab'));
-addpath(genpath('/N/u/brlife/git/o3d-code'));
-addpath(genpath('/N/u/brlife/git/encode'));
+if ~isdeployed
+	addpath(genpath('/N/u/brlife/git/vistasoft'));
+	addpath(genpath('/N/u/brlife/git/jsonlab'));
+	addpath(genpath('/N/u/brlife/git/o3d-code'));
+end
 
 config = loadjson('config.json');
 ref_src = fullfile(config.t1_moving);
@@ -16,17 +17,17 @@ load(fullfile(config.segmentation));
 fid=fopen('tract_name_list.txt', 'w');
 
 if (config.tract1 > 0)
-    for tract = [config.tract1, config.tract2, config.tract3, config.tract4]
+    for tract = [config.tract1, config.tract2, config.tract3, config.tract4, config.tract5, config.tract6, config.tract7, config.tract8]
         if (tract > 0)
             tract_name=strrep(fg_classified(tract).name,' ','_');
-            write_fg_to_trk(fg_classified(tract),ref_src,sprintf('%s_tract.trk',tract_name));
+            write_fg_to_trk_shift(fg_classified(tract),ref_src,sprintf('%s_tract.trk',tract_name));
             fprintf(fid, [tract_name, '\n']);
         end    
     end    
 else
-    for tract=1:20
+    for tract=1:length(fg_classified)
         tract_name=strrep(fg_classified(tract).name,' ','_');
-        write_fg_to_trk(fg_classified(tract),ref_src,sprintf('%s_tract.trk',tract_name));
+        write_fg_to_trk_shift(fg_classified(tract),ref_src,sprintf('%s_tract.trk',tract_name));
         fprintf(fid, [tract_name, '\n']);  
     end 
 end
